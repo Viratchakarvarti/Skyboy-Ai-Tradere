2fcf2bfab808fffaecf161b654fa114b96086cb4
Viratchakarvarti <viratchakarvarti5@gmail.com>
2026-05-22 20:05:24 +0530

    Update chart_engine.py

--- chart_engine.py
+++ chart_engine.py
@@ -1,6 +1,7 @@
 import plotly.graph_objects as go
 import streamlit as st
 
-def draw_chart(df, ticker):
-    fig = go.Figure(data=[go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])])
+def draw(df):
+    fig = go.Figure(data=[go.Candlestick(x=df.index[-100:], open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])])
+    fig.update_layout(title="Live Candlestick Chart", xaxis_rangeslider_visible=False)
     st.plotly_chart(fig, use_container_width=True)
