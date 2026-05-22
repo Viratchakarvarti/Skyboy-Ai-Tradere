263beb6c64cfb25fbd11b0924e30a998ac1c2ddb
Viratchakarvarti <viratchakarvarti5@gmail.com>
2026-05-22 20:04:47 +0530

    Change function to get_data with caching and longer period
    Refactor stock data retrieval to use Streamlit caching and extend the data period.

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,6 @@
 import yfinance as yf
-def get_stock_data(symbol):
-    return yf.download(symbol, period="5d", interval="5m")
-  
+import streamlit as st
+
+@st.cache_data(ttl=3600)
+def get_data(symbol):
+    return yf.download(symbol, period="15y", interval="5m")
